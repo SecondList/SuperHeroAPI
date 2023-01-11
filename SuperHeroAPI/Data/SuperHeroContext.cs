755e15903ec0d96defd4e6e3e060bf11fc38cad1
fmasm <fmasm@DESKTOP-5IBSP1F>
2023-01-11 16:03:39 +0800

    Add project files.

---
+++ SuperHeroAPI/Data/SuperHeroContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI.Models;
+
+namespace SuperHeroAPI.Data
+{
+    public class SuperHeroContext :DbContext
+    {
+        public SuperHeroContext(DbContextOptions<SuperHeroContext> options) : base(options) { }
+
+        public DbSet<SuperHero> SuperHeros { get; set; }
+    }
+}
---
+++ SuperHeroAPI/Models/SuperHero.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperHeroAPI.Models
+{
+    public class SuperHero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+    }
+}
